ae853ffea0a3068da625c4015c85d287532daa6c
sohnyi <sohnyi@163.com>
2023-10-12 14:16:51 +0800

    iml delete function

--- app/src/main/java/com/sohnyi/uninstall/MainActivity.kt
+++ app/src/main/java/com/sohnyi/uninstall/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.sohnyi.uninstall
 
+import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sohnyi.uninstall.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
@@ -13,11 +15,12 @@ import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val adapter by lazy {
-        PackageAdapter()
+        PackageAdapter(::onDeleteClicked)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,29 +28,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.pb.visibility = View.VISIBLE
-
         binding.packageList.layoutManager = LinearLayoutManager(this)
         binding.packageList.adapter = adapter
 
-        MainScope().launch(Dispatchers.IO) {
-            val pList = getAllPackages()
-            withContext(Dispatchers.Main) {
-                adapter.submitList(pList)
-                binding.pb.visibility = View.GONE
-            }
-        }
+        updatePackages()
+
     }
 
-    // FIXME: can not get all packages
-    // FIXME: getInstalledPackages() Deprecated
-    private fun getAllPackages(): List<PackageInfo> {
+    private fun updatePackages() {
+        binding.pb.visibility = View.VISIBLE
         val packageInfoList = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             packageManager.getInstalledPackages(PackageManager.PackageInfoFlags.of(PackageManager.GET_META_DATA.toLong()))
         } else {
             packageManager.getInstalledPackages(PackageManager.GET_META_DATA)
         }
 
-        return packageInfoList
+        MainScope().launch(Dispatchers.IO) {
+            withContext(Dispatchers.Main) {
+                adapter.submitList(packageInfoList)
+                binding.pb.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun onDeleteClicked(pi: PackageInfo) {
+        try {
+            startActivity(Intent(Intent.ACTION_UNINSTALL_PACKAGE).apply {
+                data = Uri.parse("package:${pi.packageName}")
+            })
+            updatePackages()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
--- app/src/main/java/com/sohnyi/uninstall/PackageAdapter.kt
+++ app/src/main/java/com/sohnyi/uninstall/PackageAdapter.kt
@@ -8,7 +8,8 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.sohnyi.uninstall.databinding.ItemPackageBinding
 import java.lang.Exception
 
-class PackageAdapter : ListAdapter<PackageInfo, PackageAdapter.PackageHolder>(PackageDiffCallback) {
+class PackageAdapter(private val onDeleteClicked: (pi: PackageInfo) -> Unit) :
+    ListAdapter<PackageInfo, PackageAdapter.PackageHolder>(PackageDiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PackageHolder {
         val inflate = LayoutInflater.from(parent.context)
@@ -17,17 +18,19 @@ class PackageAdapter : ListAdapter<PackageInfo, PackageAdapter.PackageHolder>(Pa
     }
 
     override fun onBindViewHolder(holder: PackageHolder, position: Int) {
-        holder.bind(getItem(position))
+        holder.bind(getItem(position), onDeleteClicked)
     }
 
     inner class PackageHolder(private val binding: ItemPackageBinding) : ViewHolder(binding.root) {
-        fun bind(pi: PackageInfo) {
+        fun bind(pi: PackageInfo, onDeleteClicked: (pi: PackageInfo) -> Unit) {
             try {
                 val pm = binding.root.context.packageManager
                 binding.ivIcon.setImageDrawable(pm.getApplicationIcon(pi.applicationInfo))
                 binding.name.text = pm.getApplicationLabel(pi.applicationInfo)
                 binding.id.text = pi.packageName
-                // TODO: delete function to iml
+                binding.ibUninstall.setOnClickListener {
+                    onDeleteClicked(pi)
+                }
             } catch (e: Exception) {
                 e.printStackTrace()
             }
